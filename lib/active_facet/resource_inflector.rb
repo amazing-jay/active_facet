71d6b89151ff97051ade56166193a8475efb806e
Jay Crouch <i.jaycrouch@gmail.com>
2016-04-22 15:53:44 -0500

    RCB updating v#version

---
+++ lib/active_facet/resource_inflector.rb
@@ -0,0 +1,53 @@
+module ActiveFacet
+  module ResourceInflector
+    extend ActiveSupport::Concern
+
+    def resource_class
+      raise 'abstract method (implement me!)'
+    end
+
+    def resource_attribute_name(field)
+      raise 'abstract method (implement me!)'
+    end
+
+    # Constantizes an appropriate resource serializer class for relations
+    # @param field [Symbol] to find relation reflection for
+    # @return [Reflection | nil]
+    def get_association_reflection(field)
+      @association_reflections ||= {}
+      @association_reflections[field] ||= resource_class.reflect_on_association(resource_attribute_name(field).to_sym)
+    end
+
+    # Constantizes an appropriate resource serializer class
+    # @param field [Symbol] to test as relation and find serializer class for
+    # @return [Class | nil]
+    def get_association_serializer_class(field, options)
+      @association_serializers ||= {}
+      unless @association_serializers.key? field
+        @association_serializers[field] = nil
+        #return nil if field isn't an association
+        if reflection = get_association_reflection(field)
+          #return nil if association doesn't have a custom class
+          @association_serializers[field] = ActiveFacet::Helper.serializer_for(reflection.klass, options)
+        end
+      end
+      @association_serializers[field]
+    end
+
+    # Constantizes an appropriate attribute serializer class
+    # @param attribute [Symbol] base_name of attribute serializer class to find
+    # @param options [Hash]
+    # @return [Class | nil]
+    def get_custom_serializer_class(attribute, options)
+      @custom_serializers ||= {}
+      @custom_serializers[attribute] ||= ActiveFacet::Helper.attribute_serializer_class_for(resource_class, attribute, options)
+    end
+
+    # Determines if public attribute maps to a private relation
+    # @param field [Symbol] public attribute name
+    # @return [Boolean]
+    def is_association?(field)
+      !!get_association_reflection(field)
+    end
+  end
+end
---
+++ spec/active_facet/resource_inflector_spec.rb
@@ -0,0 +1,84 @@
+require 'spec_helper'
+
+describe ActiveFacet::ResourceInflector do
+
+  let(:resource_serializer_class) { V1::ResourceA::ResourceASerializer }
+  let(:association_serializer_class) { V1::ResourceB::ResourceBSerializer }
+  let(:attribute_serializer_class) { V1::CustomizerAttributeSerializer }
+  let(:instance) { Class.new {
+    include ActiveFacet::ResourceInflector
+    def resource_class
+      ResourceA
+    end
+    def resource_attribute_name(field)
+      field
+    end
+  }.new }
+
+  describe ".get_association_reflection" do
+    subject { instance.get_association_reflection(field) }
+
+    context "relation" do
+      let(:field) { :parent }
+      it { expect(subject.class).to be(ActiveRecord::Reflection::AssociationReflection) }
+    end
+
+    context "field" do
+      let(:field) { :explicit_attr }
+      it { expect(subject).to be nil }
+    end
+  end
+
+  describe ".get_association_serializer_class" do
+    let(:options) { }
+    subject { instance.get_association_serializer_class(field, options) }
+    context "self association" do
+      let(:field) { :parent }
+      it { expect(subject).to be(resource_serializer_class.instance) }
+    end
+
+    context "association" do
+      let(:field) { :master }
+      it { expect(subject).to be(association_serializer_class.instance) }
+    end
+
+    context "attribute" do
+      let(:field) { :explicit_attr }
+      it { expect(subject).to be nil }
+    end
+
+    skip "todo: test version impact in lookups"
+  end
+
+  describe ".get_custom_serializer_class" do
+    let(:options) { }
+    subject { instance.get_custom_serializer_class(field, options) }
+    context "registered" do
+      let(:field) { :customizer }
+      it { expect(subject).to be(attribute_serializer_class) }
+    end
+
+    context "unregistered" do
+      let(:field) { :unregistered }
+      skip { expect{subject}.to raise_error(ActiveFacet::Errors::LookupError, 'Unable to locate serializer for:: ["ResourceA", "Unregistered", :attribute_serializer, 1.0, nil]') }
+    end
+  end
+
+  describe ".is_association?" do
+    subject { instance.is_association?(field) }
+    context "registered" do
+      let(:field) { :parent }
+      it { expect(subject).to be true }
+    end
+
+    context "unregistered" do
+      let(:field) { :extras }
+      it { expect(subject).to be true }
+    end
+
+    context "attribute" do
+      let(:field) { :explicit_attr }
+      it { expect(subject).to be false }
+    end
+  end
+end
